720c800d2ab8a777977a3138a32c6c54467108ce
saif64s <my@wo.rk>
2023-08-17 14:46:11 +0600

    plutoGrid does not update

--- lib/screens/tab_screens/breakdown.dart
+++ lib/screens/tab_screens/breakdown.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pluto_grid/pluto_grid.dart';
+import 'package:pricing_structure/consts/breakdown_vars.dart';
+
+import 'day_header.dart';
 
 class BreakDown extends ConsumerStatefulWidget {
   const BreakDown({super.key});
@@ -8,9 +12,58 @@ class BreakDown extends ConsumerStatefulWidget {
   ConsumerState<ConsumerStatefulWidget> createState() => _BreakDownState();
 }
 
-class _BreakDownState extends ConsumerState<BreakDown> {
+class _BreakDownState extends ConsumerState<BreakDown>
+    with AutomaticKeepAliveClientMixin {
+  final _moduleNameController = TextEditingController();
+  int inputText = 0;
+  late PlutoGridStateManager stateManager;
+
+  @override
+  void dispose() {
+    _moduleNameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    super.build(context);
+    // final _index = ModalRoute.of(context)!.settings.arguments;
+
+    // return Center(
+    //   child: Column(
+    //     children: [
+    //       TextField(
+    //         onChanged: (value) {
+    //           ref.read(dayProvider.notifier).changeNumber(int.tryParse(value)!);
+    //         },
+    //         controller: _moduleNameController,
+    //         decoration: InputDecoration(
+    //             border: OutlineInputBorder(),
+    //             hintText: "${ref.watch(dayProvider)}"),
+    //       ),
+    //       Text("${ref.watch(dayProvider)}")
+    //     ],
+    //   ),
+    // );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 10),
+      child: PlutoGrid(
+        columns: breakDownCols,
+        rows: breakdownRows,
+        onChanged: (PlutoGridOnChangedEvent event) {
+          print(event);
+
+          stateManager.notifyListeners();
+        },
+        onLoaded: (PlutoGridOnLoadedEvent event) async {
+          stateManager = event.stateManager;
+        },
+        createHeader: (stateManager) => DayHeader(stateManager: stateManager),
+      ),
+    );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
