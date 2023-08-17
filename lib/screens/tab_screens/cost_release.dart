2bba670cf5603910bd72d2f4fda744a84ed9ff0a
saif64s <my@wo.rk>
2023-08-17 14:12:46 +0600

    table added but still needs fixing

--- lib/screens/tab_screens/cost_release.dart
+++ lib/screens/tab_screens/cost_release.dart
@@ -5,51 +5,200 @@ class CostRelease extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ExpansionTile(
-          title: Text(
-            "Pricing Calc",
-          ),
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Column(
           children: [
-            Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.amber,
+            ExpansionTile(
+              title: const Text(
+                "Pricing Calc",
+              ),
+              children: [
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: Colors.amber,
+                ),
+              ],
+            ),
+            ExpansionTile(
+              title: Text(
+                "No. of Dev Resources",
+              ),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: DataTable(
+                    dataRowMaxHeight: 25,
+                    dataRowMinHeight: 20,
+                    border: TableBorder.all(),
+                    columns: [
+                      DataColumn(label: Text("Dev Resources:")),
+                      DataColumn(label: Text("Number")),
+                    ],
+                    rows: [
+                      DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              "BE",
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              "1",
+                            ),
+                          ),
+                        ],
+                      ),
+                      DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              "FE",
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              "0",
+                            ),
+                          ),
+                        ],
+                      ),
+                      DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              "FS",
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              "0",
+                            ),
+                          ),
+                        ],
+                      ),
+                      DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              "BA",
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              "1",
+                            ),
+                          ),
+                        ],
+                      ),
+                      DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              "QA",
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              "0",
+                            ),
+                          ),
+                        ],
+                      ),
+                      DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              "UX",
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              "0",
+                            ),
+                          ),
+                        ],
+                      ),
+                      DataRow(
+                        cells: [
+                          DataCell(
+                            Text(
+                              "MOCH/Sales",
+                            ),
+                          ),
+                          DataCell(
+                            Text(
+                              "YES",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            const ExpansionTile(
+              childrenPadding: EdgeInsets.all(16),
+              title: Text(
+                "Margins",
+              ),
+              children: [
+                Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Gross Profit Margin (SELISE)",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                    SizedBox(height: 10),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Net Profit Margin (WEB)",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                        ),
+                      ),
+                      keyboardType: TextInputType.number,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const ExpansionTile(
+              title: Text(
+                "Initial Sales Rate Total cost w/ Overheads",
+              ),
+            ),
+            const ExpansionTile(
+              title: Text(
+                "Automatic Calc with % discount",
+              ),
+            ),
+            const ExpansionTile(
+              title: Text(
+                "Automatic Calc- 10,000 Differences",
+              ),
+            ),
+            const ExpansionTile(
+              title: Text(
+                "Manual Calculator(User Inputs)",
+              ),
             ),
           ],
         ),
-        ExpansionTile(
-          title: Text(
-            "No. of Dev Resources",
-          ),
-        ),
-        ExpansionTile(
-          title: Text(
-            "Margins",
-          ),
-        ),
-        ExpansionTile(
-          title: Text(
-            "Initial Sales Rate Total cost w/ Overheads",
-          ),
-        ),
-        ExpansionTile(
-          title: Text(
-            "Automatic Calc with % discount",
-          ),
-        ),
-        ExpansionTile(
-          title: Text(
-            "Automatic Calc- 10,000 Differences",
-          ),
-        ),
-        ExpansionTile(
-          title: Text(
-            "Manual Calculator(User Inputs)",
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
